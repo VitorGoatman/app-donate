e65dc80fb06307b4899542d3dcf2e250fcc4b7c6
VitorGoatman <vitor.goatman@gmail.com>
2020-08-12 15:37:12 -0300

    Adicionar Arquivos do projeto

---
+++ Splashscreen/obj/Debug/90/android/src/crc64fede50711094c3f8/SplasScreen.java
@@ -0,0 +1,47 @@
+package crc64fede50711094c3f8;
+
+
+public class SplasScreen
+	extends android.app.Activity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Splashscreen.SplasScreen, Splashscreen", SplasScreen.class, __md_methods);
+	}
+
+
+	public SplasScreen ()
+	{
+		super ();
+		if (getClass () == SplasScreen.class)
+			mono.android.TypeManager.Activate ("Splashscreen.SplasScreen, Splashscreen", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
